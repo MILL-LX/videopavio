2278ea01b00c5b4c30e9418582776dbcea9a7fdd
pauloandringa <75536480+pauloandringa@users.noreply.github.com>
2025-10-10 14:48:23 +0100

    Update grava_com_bash.sh

--- recorder/grava_com_bash.sh
+++ recorder/grava_com_bash.sh
@@ -7,4 +7,4 @@ x=`date +%F_%H%M%S`;
 echo $x;
 
 rpicam-vid --timeout=10000 --width=1920 --height=1080 --nopreview --bitrate=15000k --framerate=24 --low-latency --codec=libav -o /home/pi/videopavio/videos/$x.mp4 && 
-rsync --recursive --update --verbose /home/pi/videopavio/videos/*4 pi@opencv.local:videopavio/videos/
+rsync -e "ssh -i /home/pi/.ssh/THE_PRIVATE_KEY_NAME" --recursive --update --verbose /home/pi/videopavio/videos/*4 pi@opencv.local:videopavio/videos/
