5029704d64686a48329abb9f3ed49bcb275eb54c
pauloandringa <75536480+pauloandringa@users.noreply.github.com>
2025-10-14 13:24:37 +0100

    Create gpiozero_sensor.py

---
+++ server/gpiozero_sensor.py
@@ -0,0 +1,30 @@
+from time import sleep
+from gpiozero import Button
+from signal import pause
+import socketio
+
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print('connection established')
+
+button = Button(4)
+
+def on_button_pressed():
+    if (button.is_pressed):
+        print("sensor detected!")
+        sio.emit('record', 'now')
+        sleep(35) # wait a little more than the video timeout!
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+sio.connect('http://videopavio.local:5000')
+
+# # Add the event listener
+button.when_pressed = on_button_pressed
+pause()
+
+sio.wait()
