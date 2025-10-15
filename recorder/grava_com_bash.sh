5b9602cce0af670dee0c7a9a3f1acf060bbc28cf
pauloandringa <75536480+pauloandringa@users.noreply.github.com>
2025-10-15 16:55:44 +0100

    Update grava_com_bash.sh
    usb drive rsync

--- recorder/grava_com_bash.sh
+++ recorder/grava_com_bash.sh
@@ -3,11 +3,13 @@
 echo "recording file: ";
 
 x=`date +%F_%H%M%S`;
-timeout=500000;
+# timeout=500000;
 # timeout=1800000;
 # timeout=30000;
+timeout=540000;
+
 
 echo $x;
 
-rpicam-vid --timeout=$timeout --width=1920 --height=1080 --nopreview --bitrate=11000k --framerate=24 -o /home/pi/videopavio/videos/$x.mp4 && 
-rsync -e "ssh -i /home/pi/.ssh/id_rsa" --recursive --update --verbose /home/pi/videopavio/videos/*4 pi@videopavio.local:videopavio/videos/
+rpicam-vid --hflip --timeout=$timeout --width=1920 --height=1080 --nopreview --bitrate=9000k --framerate=24 --low-latency --codec=libav --profile high --level 4.2 --denoise cdn_fast --libav-audio 0 -o /home/pi/videopavio/videos/$x.mp4 && 
+rsync -e "ssh -i /home/pi/.ssh/id_rsa" --recursive --update --verbose --progress /home/pi/videopavio/videos/*4 pi@videopavio.local:/media/pi/4BCF-8A8C/videopavio/videos/
